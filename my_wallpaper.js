83d6b05c590b3e0c4898e5a89ca0f370fd9e72eb
Xueting Piao <charcypark9384@gmail.com>
2025-07-29 10:22:28 +1200

    29 july

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,12 +1,19 @@
 //your parameter variables go here!
-let orangeR = orangeCx = orangeCy=66;
+
+let orangeSize=30;
+let shinePos=30
+let showShine=false;
+let x=30
+let y=30
+
+
 
 
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  //pWallpaper.output_mode(GRID_WALLPAPER);
+  //pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
@@ -22,49 +29,40 @@ function wallpaper_background() {
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-//orange 
-stroke(254,235,133);
-strokeWeight(3.5);
-
-
-ellipse(orangeCx,orangeCy,orangeR,orangeR);
-fill(254,235,133);
-stroke('white');
-strokeWeight(0.3);
-ellipse(orangeCx,orangeCy,orangeR-1,orangeR-1);
-
-stroke(254,245,194);
-strokeWeight(2);
-fill(254,245,194);
-ellipse(orangeCx,orangeCy,orangeR/10,orangeR/10);
-strokeWeight(1);
-line(orangeCx,orangeCy,orangeCx,orangeCy-orangeR/2+1.3);
-line(orangeCx,orangeCy,orangeCx*1.34,orangeCy*1.34)
-
+//draw orange
 push()
+fill(255,165,0);
+stroke(210,105,30);
+strokeWeight(2)
+ellipse(x,y,orangeSize,orangeSize);
+stroke('wihte')
+strokeWeight(0.2)
+ellipse(x,y,orangeSize-1,orangeSize-1);
+stroke(210,117,86)
+strokeWeight(1)
+point(x*1.3,orangeSize*1.2)
+point(x*1.2,orangeSize*1.1)
+point(x*1.2,orangeSize*1.2)
 
-translate(orangeCx,orangeCy)
-line(0,0,0,orangeCy-orangeR/2-1);
-rotate(45)
-line(0,0,0,orangeCy-orangeR/2-1);
-rotate(90)
+if (showShine){
+  let shineX=x-orangeSize/4+shinePos/50;
+  let shineY=y-orangeSize/4;
+  fill(255,255,200,180)
+  noStroke();
+  ellipse(shineX,shineY,orangeSize/3,orangeSize/3);
 
-line(0,0,0,orangeCy-orangeR/2-1);
-rotate(135)
-line(0,0,0,orangeCy-orangeR/2-1);
-rotate(0)
-line(0,0,0,orangeCy-orangeR/2-1);
-
-line(0,0,0,orangeCy-orangeR/2-1);
-rotate(-45)
-line(0,0,0,orangeCy-orangeR/2-1);
-rotate(-135)
-
-line(0,0,0,orangeCy-orangeR/2-1);
-rotate(90)
-
+}
+//draw leaves
+fill(34,139,34);
+noStroke();
+triangle(x-5,y-orangeSize/2,x-15,y-orangeSize/2-15,x-5,y-orangeSize/2-10)
 
 pop()
+
+
+
+
+
 
 
 
