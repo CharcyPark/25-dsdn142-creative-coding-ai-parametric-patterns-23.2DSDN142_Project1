68c228627e696db7a52204fdcaa57f2e8a089beb
Xueting Piao <charcypark9384@gmail.com>
2025-08-03 17:50:12 +1200

    3 AUG Wallpaper 6 fireworks

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,14 +1,10 @@
 //your parameter variables go here!
-//position control
-let midx=100
-let midy=100
-//size control
-let outerR=50
-let innerR=outerR*0.7
-//corner style control
-let smoothness=0.8
-//if control eye direction to show different emotions
-let emotionType=1 //0,1,otherNum
+let W=200
+let H=200
+let fireLightSize;
+let fireCount=33
+let fireworkSize=20
+let fireworkNum=80
 let c1;
 let c2;
 let c3;
@@ -18,13 +14,13 @@ let c6;
 let c7;
 let c8;
 let c9;
-let flowerSize=6//background stars
-let flowernum=200//background stars
+
+
 function setup_wallpaper(pWallpaper) {
   //pWallpaper.output_mode(DEVELOP_GLYPH);
   pWallpaper.output_mode(GRID_WALLPAPER);
   
-  pWallpaper.resolution(FIT_TO_SCREEN);
+  pWallpaper.resolution(A3);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
@@ -37,255 +33,170 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(245, 193, 233);
-
- 
+  background(17,24,42); 
+  
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-c1=color(217, 152, 187,150)//pink light
-  c2=color(207, 131, 235,131)//light purple
-  c3=color(199, 124, 214,120)//dark purple
-  c4=color(184, 123, 170,180)//light pink
-  c5=color(255, 94, 167,90)//red pink
-  c6=color(224, 245, 157,160)//yello green light
-  c7=color(235, 163, 108,200)//orange red
-  c8=color(237, 152, 126,180)//light red
-  c9=color(245, 103, 174,200)//pink dark
-  let myColors=[c1,c2,c3,c4,c5,c6,c7,c8,c9];
-  let randomColor=random(myColors)
- //draw background stars
- strokeWeight(0.2)
- fill(randomColor) 
-for(i=0;i<flowernum;i++){
-    let xPos=random(10,195)
-    let yPos=random(10,195)
- 
- push()
- 
-translate(xPos,yPos)
+
+fireLightSize=random(1,2)
+
+setup_drawSky();
+
+
+}
+
+
+function setup_drawSky(){
+let from= color(17,24,42)
+let to =color(2,26,42)
+let interA= lerpColor(from,to,0.25)
+let interB= lerpColor(from,to,0.3)
+noStroke()
+fill(from)
+rect(0,0,W,H/2)//top rec
+
+fill(interA)
+rect(0,H/2,W,H/4)
+
+fill(interB)
+rect(0,H*3/4,W,H/8)
+
+fill(to)
+rect(0,H*7/8,W,H/8)
+// draw points
+let col1=(202, 245, 64)
+let col2=(51, 24, 204)
+let col3=(236, 235, 242)
+let col4=(227, 121, 34)
+let groupcolor=[col1,col2,col3,col4]
+let randomColor1=color(groupcolor)
+stroke(randomColor1)
+strokeWeight(1)
+for (let i=0; i<500; i++) {
+  let x=random(1,W-1)
+  let y=random(1,H*3/4)
+  point(x,y)
+}
+
+
+setup_drawFires()
+}
+
+
+function setup_drawFires(){
   
-drawbackgroundflower()
-pop()}
-function drawbackgroundflower(){
-stroke(randomColor)
+
+  angleMode(DEGREES)
+//draw many fire light flying
+for (let i=0; i<fireCount;i++) {
+  let xPos=random(20,W-20);
+  let yPos=random(H/2,H*6/8);//random x,y but in the up part
+  push()
+  translate(xPos,yPos);
+  
+  rotate(-150)//turning 
+  drawFlyingFire();//draw sigle fire
+
+  pop();
+  
+}drawFireworksN()}
+function drawFireworksN(){
+    angleMode(DEGREES)
+  for (let i=0; i<fireworkNum;i++) {
+  let xPos1=random(20,W-20);
+  let yPos1=random(20,H/2);//random x,y but in the up part
+  push()
+  translate(xPos1,yPos1);
+  
+  rotate(-150)//turning 
+  drawFireworks();//draw sigle fire
+  pop()
+}
+
+  
+
+
+  fill(220,200,200,150)
+  ellipse(0,0,fireworkSize/2,fireworkSize/2)
+
+}
+function drawFlyingFire(){
+  c1=color(154,122,204,150)
+c2=color(204,122,131,131)
+c3=color(122,131,204,120)
+c4=color(151,209,145,180)
+c5=color(155,210,146,80)
+c6=color(220,209,131,160)
+c7=color(159,226,245,200)
+c8=color(122,131,204,180)
+c9=color(231,244,78,200)
+let myColors=[c1,c2,c3,c4,c5,c6,c7,c8,c9];
+  let randomColor=random(myColors)
+  stroke(randomColor)
  strokeWeight(1)
  fill(randomColor)
- 
- push() 
- rotate(36)
+ beginShape()
+ curveVertex(0,0)
+ curveVertex(0,0)
+ curveVertex(0,0)
+ curveVertex(-10,-22)
+ curveVertex(-30,-50)
+ curveVertex(-30,-50)
 
+ endShape()
+ push() 
+ rotate(45)
+ strokeWeight(0.2)
+ fill(randomColor,random(150,200)) 
   for (let i=0; i<5; i++) {
-ellipse(0,0,flowerSize/3,flowerSize*1.5)
+ellipse(0,0,fireLightSize/3,fireLightSize*1.5)
+noStroke()
+
+fill(randomColor,210)
+ellipse(0,0,fireLightSize/3+2,fireLightSize*1.5+2)
+
 rotate(72)
   } 
   fill(220,200,200,200)
-  ellipse(0,0,flowerSize/2,flowerSize/2) 
-  pop()
-}
-  
- 
- strokeWeight(0.2)
- fill(randomColor) 
-drawFlower()
+  ellipse(0,0,fireLightSize/2,fireLightSize/2) 
+  pop()}
 
-  }
-  function drawFlower() {
-  c1=color(217, 152, 187,150)//pink light
-  c2=color(207, 131, 235,131)//light purple
-  c3=color(59, 75, 184,120)//dark blue
-  c4=color(125, 199, 240,180)//light blue
-  c5=color(145, 245, 125,90)//yellow green
-  c6=color(219, 205, 96,160)//yello orange
-  c7=color(235, 163, 108,200)//orange red
-  c8=color(237, 152, 126,180)//light red
-  c9=color(245, 103, 174,200)//pink dark
-  let myColors=[c1,c2,c3,c4,c5,c6,c7,c8,c9];
+function drawFireworks(){
+  angleMode(DEGREES)
+  c1=color(154,122,204,150)
+c2=color(204,122,131,131)
+c3=color(122,131,204,120)
+c4=color(151,209,145,180)
+c5=color(155,210,146,80)
+c6=color(220,209,131,160)
+c7=color(159,226,245,200)
+c8=color(122,131,204,180)
+c9=color(231,244,78,200)
+let myColors=[c1,c2,c3,c4,c5,c6,c7,c8,c9];
   let randomColor=random(myColors)
+ push() 
+ rotate(45)
+ strokeWeight(0.2)
+ fill(randomColor,random(150,200))
+  
+  for (let i=0; i<10; i++) {
+ellipse(0,0,fireworkSize/8,fireworkSize*4)
 noStroke()
 
-   translate(midx,midy)
-   
-   angleMode(DEGREES)
-   beginShape()
-      strokeWeight(0.2)
-      fill(232, 135, 188) 
-       
-
-      let firstOuterX = outerR * cos(-90);
-      let firstOuterY = outerR * sin(-90);
-      vertex(firstOuterX, firstOuterY);
-      for(let i=0;i<5;i++) {
-        //outpoint(present)
-        let outerAngle = 360 * i / 5 -180;
-        let outerX = outerR * cos(outerAngle);
-        let outerY = outerR * sin(outerAngle);
-        //innerpoint(present)
-        let innerAngle = 360 * (i + 0.5) / 5 -180;
-        let innerX = innerR * cos(innerAngle);
-        let innerY = innerR * sin(innerAngle);
-        //nextinnerpoint
-        let nextInnerAngle = 360* (i + 1.5) / 5-90;
-        let nextInnerX = innerR * cos(nextInnerAngle);
-        let nextInnerY = innerR * sin(nextInnerAngle);
-        //caculatet the control point(from inner-outpoint)
-        let cp1x = lerp(innerX, outerX, smoothness);
-        let cp1y = lerp(innerY, outerY, smoothness);
-        let cp2x = lerp(outerX, nextInnerX, smoothness);
-        let cp2y = lerp(outerY, nextInnerY, smoothness);
-        //drawbezier1
-        bezierVertex(cp1x, cp1y, cp2x, cp2y, nextInnerX, nextInnerY)
-        //draw next outpoint
-        let nextOuterAngle = 360 * (i+1) / 5 -90;
-        let nextOuterX = outerR * cos(nextOuterAngle);
-        let nextOuterY = outerR * sin(nextOuterAngle);
-        //caculate
-        let cp3x = lerp(nextInnerX, nextOuterX, smoothness);
-        let cp3y = lerp(nextInnerY, nextOuterY, smoothness);
-        //draw another bezier
-        bezierVertex(cp3x, cp3y, cp3x, cp3y, nextOuterX, nextOuterY);
-      }
-      
-      endShape(CLOSE);
-      //draw shadow
-      beginShape()
-      fill(235, 138, 190,200) 
-
-      let firstOuterX1 = (outerR+2) * cos(-90);
-      let firstOuterY1 = (outerR+2) * sin(-90);
-      vertex(firstOuterX1, firstOuterY1);
-      for(let i=0;i<5;i++) {
-        //outpoint(present)
-        let outerAngle = 360 * i / 5 -180;
-        let outerX1 = (outerR+2) * cos(outerAngle);
-        let outerY1 = (outerR+2) * sin(outerAngle);
-        //innerpoint(present)
-        let innerAngle = 360 * (i + 0.5) / 5 -180;
-        let innerX1 = (innerR+2) * cos(innerAngle);
-        let innerY1 = (innerR+2) * sin(innerAngle);
-        //nextinnerpoint
-        let nextInnerAngle = 360* (i + 1.5) / 5-90;
-        let nextInnerX1 = (innerR+2) * cos(nextInnerAngle);
-        let nextInnerY1 = (innerR+2) * sin(nextInnerAngle);
-        //caculatet the control point(from inner-outpoint)
-        let cp1x = lerp(innerX1, outerX1, smoothness);
-        let cp1y = lerp(innerY1, outerY1, smoothness);
-        let cp2x = lerp(outerX1, nextInnerX1, smoothness);
-        let cp2y = lerp(outerY1, nextInnerY1, smoothness);
-        //drawbezier1
-        bezierVertex(cp1x, cp1y, cp2x, cp2y, nextInnerX1, nextInnerY1)
-        //draw next outpoint
-        let nextOuterAngle = 360 * (i+1) / 5 -90;
-        let nextOuterX1 = (outerR+2) * cos(nextOuterAngle);
-        let nextOuterY1 = (outerR+2) * sin(nextOuterAngle);
-        //caculate
-        let cp3x = lerp(nextInnerX1, nextOuterX1, smoothness);
-        let cp3y = lerp(nextInnerY1, nextOuterY1, smoothness);
-        //draw another bezier
-        bezierVertex(cp3x, cp3y, cp3x, cp3y, nextOuterX1, nextOuterY1);
-      }
-      
-      endShape(CLOSE);
-      //draw highlight
-      beginShape()
-      fill(250, 167, 209) 
-
-      let firstOuterX2 = (outerR-10) * cos(-90);
-      let firstOuterY2 = (outerR-10) * sin(-90);
-      vertex(firstOuterX2, firstOuterY2);
-      for(let i=0;i<5;i++) {
-        //outpoint(present)
-        let outerAngle = 360 * i / 5 -180;
-        let outerX2 = (outerR-10) * cos(outerAngle);
-        let outerY2 = (outerR-10) * sin(outerAngle);
-        //innerpoint(present)
-        let innerAngle = 360 * (i + 0.5) / 5 -180;
-        let innerX2 = (innerR-10) * cos(innerAngle);
-        let innerY2 = (innerR-10) * sin(innerAngle);
-        //nextinnerpoint
-        let nextInnerAngle = 360* (i + 1.5) / 5-90;
-        let nextInnerX2 = (innerR-10) * cos(nextInnerAngle);
-        let nextInnerY2 = (innerR-10) * sin(nextInnerAngle);
-        //caculatet the control point(from inner-outpoint)
-        let cp1x = lerp(innerX2, outerX2, smoothness);
-        let cp1y = lerp(innerY2, outerY2, smoothness);
-        let cp2x = lerp(outerX2, nextInnerX2, smoothness);
-        let cp2y = lerp(outerY2, nextInnerY2, smoothness);
-        //drawbezier1
-        bezierVertex(cp1x, cp1y, cp2x, cp2y, nextInnerX2, nextInnerY2)
-        //draw next outpoint
-        let nextOuterAngle = 360 * (i+1) / 5 -90;
-        let nextOuterX2 = (outerR-10) * cos(nextOuterAngle);
-        let nextOuterY2 = (outerR-10) * sin(nextOuterAngle);
-        //caculate
-        let cp3x = lerp(nextInnerX2, nextOuterX2, smoothness);
-        let cp3y = lerp(nextInnerY2, nextOuterY2, smoothness);
-        //draw another bezier
-        bezierVertex(cp3x, cp3y, cp3x, cp3y, nextOuterX2, nextOuterY2);
-      }
-      
-      endShape(CLOSE);
-     
+rotate(72)
+  } 
+  fill(randomColor,200)
+  ellipse(0,0,fireworkSize/8,fireworkSize/8) 
+  pop()
+}
 
 
-  drawFlowerInside()    
-    } 
-    function drawFlowerInside() {
-      beginShape()
-      fill(247, 101, 213,200) 
-
-      let firstOuterX3 = (outerR/5) * cos(-90);
-      let firstOuterY3 = (outerR/5) * sin(-90);
-      vertex(firstOuterX3, firstOuterY3);
-      for(let i=0;i<5;i++) {
-        //outpoint(present)
-        let outerAngle = 360 * i / 5 -180;
-        let outerX3 = (outerR/5) * cos(outerAngle);
-        let outerY3 = (outerR/5) * sin(outerAngle);
-        //innerpoint(present)
-        let innerAngle = 360 * (i + 0.5) / 5 -180;
-        let innerX3 = (innerR/5) * cos(innerAngle);
-        let innerY3 = (innerR/5) * sin(innerAngle);
-        //nextinnerpoint
-        let nextInnerAngle = 360* (i + 1.5) / 5-90;
-        let nextInnerX3 = (innerR/5) * cos(nextInnerAngle);
-        let nextInnerY3 = (innerR/5) * sin(nextInnerAngle);
-        //caculatet the control point(from inner-outpoint)
-        let cp1x = lerp(innerX3, outerX3, smoothness);
-        let cp1y = lerp(innerY3, outerY3, smoothness);
-        let cp2x = lerp(outerX3, nextInnerX3, smoothness);
-        let cp2y = lerp(outerY3, nextInnerY3, smoothness);
-        //drawbezier1
-        bezierVertex(cp1x, cp1y, cp2x, cp2y, nextInnerX3, nextInnerY3)
-        //draw next outpoint
-        let nextOuterAngle = 360 * (i+1) / 5 -90;
-        let nextOuterX3 = (outerR/5) * cos(nextOuterAngle);
-        let nextOuterY3 = (outerR/5) * sin(nextOuterAngle);
-        //caculate
-        let cp3x = lerp(nextInnerX3, nextOuterX3, smoothness);
-        let cp3y = lerp(nextInnerY3, nextOuterY3, smoothness);
-        //draw another bezier
-        bezierVertex(cp3x, cp3y, cp3x, cp3y, nextOuterX3, nextOuterY3);
-      }
-      
-      endShape(CLOSE);
-      
-      noFill()
-      stroke(255, 101, 200)
-      strokeWeight(1)
-      for(let i=0;i<8;i++) {
-        angleMode(DEGREES)
-        push()
-        curve(10,10,0,0,10,-10,15,-15)
-        stroke('yellow')
-        strokeWeight(2)
-        point(10,-10)
-        
 
 
-        pop()
-        rotate(45)
-      }}
+
+
+
     
 
   
